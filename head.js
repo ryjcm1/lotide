04d913da035848214a919dc3fb93ed3dac318cd3
Jimmy Chuk <jimmychuk@outlook.com>
2022-01-10 14:53:55 -0500

    added test cases

--- head.js
+++ head.js
@@ -7,6 +7,10 @@ const assertEqual = function(actual, expected) {
 };
 
 const head = (arr) => {
-  return arr[0]
-}
+  return arr[0];
+};
 
+assertEqual(head([5,6,7]), 5);
+assertEqual(head([7,8,9]), 8);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Lighthouse");
