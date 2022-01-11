800e1c77894975d2d582fb45c6718693697787a5
Jimmy Chuk <jimmychuk@outlook.com>
2022-01-11 18:54:30 -0500

    added findKey.js

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 + Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+
+const findKeyByValue = (listOfKeys, value) =>{
+
+  for (let key in listOfKeys) {
+    if (listOfKeys[key] === value) {
+      return key;
+    }
+  }
+
+  return undefined;
+};
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
