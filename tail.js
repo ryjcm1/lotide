178dd5e481b48a693c85cba7a3df3ae130e94311
Jimmy Chuk <jimmychuk@outlook.com>
2022-01-10 20:27:19 -0500

    added tail.js

--- tail.js
+++ tail.js
@@ -39,3 +39,4 @@ assertEqual(originalArr[0], result1[0]); //ensure both arrays are different
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
+
