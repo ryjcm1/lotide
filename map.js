f2a18a9c1966d19b000112a3a809fc17edbb1409
Jimmy Chuk <jimmychuk@outlook.com>
2022-01-12 22:10:00 -0500

    added takeUntil

--- map.js
+++ map.js
@@ -22,13 +22,13 @@ const assertArraysEqual = (arr1, arr2) => {
 };
 
 
-const map = function (array, callback) {
+const map = function(array, callback) {
   const results = [];
   for (let item of array) {
     results.push(callback(item));
   }
   return results;
-}
+};
 
 
 const words = ["ground", "control", "to", "major", "tom"];
