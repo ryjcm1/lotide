cb963322bb840c4e295f1bbcfa0798a61399c3c2
Jimmy Chuk <jimmychuk@outlook.com>
2022-01-12 22:28:48 -0500

    added findKey.js

---
+++ findKey.js
@@ -0,0 +1,79 @@
+const findKey = (obj, callback) => {
+
+  let keys = Object.keys(obj);
+
+  for (let key of keys) {
+
+    let current = obj[key];
+
+    if (callback(current)) {
+      return key;
+    }
+
+  }
+};
+
+
+
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+
+
+
+
+//Test cases
+
+//case where callback succeeds
+assertEqual((findKey({
+  "Blue Hill": {
+    stars: 1
+  },
+  "Akaleri": {
+    stars: 3
+  },
+  "noma": {
+    stars: 2
+  },
+  "elBulli": {
+    stars: 3
+  },
+  "Ora": {
+    stars: 2
+  },
+  "Akelarre": {
+    stars: 3
+  }
+}, x => x.stars === 2)), "noma"); // should PASS
+
+
+//case where callback fails to run true
+assertEqual((findKey({
+  "Blue Hill": {
+    stars: 1
+  },
+  "Akaleri": {
+    stars: 3
+  },
+  "noma": {
+    stars: 2
+  },
+  "elBulli": {
+    stars: 3
+  },
+  "Ora": {
+    stars: 2
+  },
+  "Akelarre": {
+    stars: 3
+  }
+}, x => x.stars === 0)), undefined); // should PASS
+
+
+//Case where object is empty
+assertEqual((findKey({}, x => x.type === "metal")), undefined); // should PASS
