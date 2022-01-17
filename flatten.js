3c24337694028bb8e34d4feb83d7a3c3d7e75abd
Jimmy Chuk <jimmychuk@outlook.com>
2022-01-17 00:34:37 -0500

    refactored flatten.js and added tests

--- flatten.js
+++ flatten.js
@@ -1,24 +1,3 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) return false;
-  else {
-    for (let i = 0; i < arr1.length - 1; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-  }
-
-  return true;
-};
-
-const assertArraysEqual = (arr1, arr2) => {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} === ${arr2}`);
-
-  }
-};
 
 const flatten = (arr1) => {
   let flattenedArray = [];
@@ -40,12 +19,4 @@ const flatten = (arr1) => {
 };
 
 
-
-//test case 1: equivalent arrays
-assertArraysEqual(flatten([1,2, [3, 4] , [5, 6, [7]]]), [1, 2, 3, 4, 5, 6, 7]); //assertion should pass
-
-//test case 2: already flatten array
-assertArraysEqual(flatten([1, 2, 3, 4, 5, 6, 7]), [1, 2, 3, 4, 5, 6, 7]); //assertion should pass
-
-//test case 3: deeply nested array
-assertArraysEqual(flatten([[1, [2, [3]]], [4,5, [6, 7, [8, 9 ]]], 10]), [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]); //assertion should pass
+module.exports = flatten;
---
+++ test/flattenTest.js
@@ -0,0 +1,27 @@
+const expect = require('chai').expect
+const flatten = require('../flatten')
+
+
+describe('flatten', () =>{
+  it('Should return the same array if passed a single-leveled array', () =>{
+    const input = [1, 2, 3, 4, 5, 6, 7];
+    const expectedOutput = [1, 2, 3, 4, 5, 6, 7];
+    expect(flatten(input)).to.deep.equal(expectedOutput)
+  });
+
+
+  it('Should return a single-leveled array when passed in a nested array', () =>{
+    const input = [1,2, [3, 4] , [5, 6, [7]]];
+    const expectedOutput = [1, 2, 3, 4, 5, 6, 7];
+    expect(flatten(input)).to.deep.equal(expectedOutput)
+  });
+
+
+  it('Should return a single-leveled array when passed a deeply nested array', () =>{
+    const input = [[1, [2, [3]]], [4,5, [6, 7, [8, 9 ]]], 10];
+    const expectedOutput = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    expect(flatten(input)).to.deep.equal(expectedOutput)
+  });
+
+
+})
